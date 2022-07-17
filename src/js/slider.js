9ceb3674aa58157fccde6d5d3fdbb15603619b0c
Kapindra <kapindra.raut@ekbana.info>
2022-07-17 22:15:18 +0545

    feat: add responsive gallery section

--- src/js/slider.js
+++ src/js/slider.js
@@ -35,3 +35,25 @@ $('.beneficiaries__cards').slick({
   mobileFirst: true,
   variableWidth: true,
 });
+
+$('.gallery__cards').slick({
+  infinite: true,
+  arrows: false,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  mobileFirst: true,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: "unslick"
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
