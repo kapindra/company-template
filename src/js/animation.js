fe748d458be00b5f9f944c5f705e9a3cf40d4e49
Kapindra <kapindra.raut@ekbana.info>
2022-07-18 20:24:52 +0545

    feat: add desktop menu modal

--- src/js/animation.js
+++ src/js/animation.js
@@ -2,9 +2,31 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+$(".desktop-nav").click(function () {
+  $(".desktop-nav").removeClass("d-lg-block");
+  // $(".custom-navbar").removeClass("custom-navbar--fixed");
+})
+
+$(".desktop-nav__contents").click(function (e) {
+  e.stopPropagation();
+})
+$(".desktop-modal-header").click(function (e) {
+  e.stopPropagation();
+})
+
+// var isClicked = $('.scholarship-link').data('clicked');
+// if (isClicked == 'yes') {
+// do something
 ScrollTrigger.create({
   start: 'top -40',
   end: 99999,
   duration: 2,
-  toggleClass: { className: 'custom-navbar--fixed', targets: '.custom-navbar' }
+  toggleClass: { className: 'custom-navbar--fixed', targets: '.normal-nav-header' }
 });
+// }
+
+$(".scholarship-link").click(function () {
+  // $(".custom-navbar").toggleClass("custom-navbar--fixed");
+  $(".desktop-nav").toggleClass("d-lg-block");
+  // $(".desktop-nav").toggleClass("custom-navbar--fixed");
+})
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,12 +1,9 @@
 $(".navbar-toggler").click(function () {
   $(".mobile-nav").toggleClass("d-none");
-  $("#white-logo").toggleClass("d-none");
-  $("#blue-logo").toggleClass("d-none");
-  $("#hamburger").toggleClass("d-none");
-  $("#close").toggleClass("d-none");
+  // $(".custom-navbar").toggleClass("custom-navbar--fixed-mobile")
 });
 
-$(".scholarship-link").click(function () {
-  $(".custom-navbar").toggleClass("custom-navbar--fixed");
-  $(".desktop-nav").toggleClass("d-lg-block");
-})
+// $(".scholarship-link").click(function () {
+//   $(".custom-navbar").toggleClass("custom-navbar--fixed");
+//   $(".desktop-nav").toggleClass("d-lg-block");
+// })
