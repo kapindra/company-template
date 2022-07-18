63234b354044ea321201d99c25b147e0c5c1dc79
Kapindra <kapindra.raut@ekbana.info>
2022-07-18 21:22:15 +0545

    fix: slider transation of mobile navmenu and button outline appearance

--- src/js/animation.js
+++ src/js/animation.js
@@ -4,7 +4,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 $(".desktop-nav").click(function () {
   $(".desktop-nav").removeClass("d-lg-block");
-  // $(".custom-navbar").removeClass("custom-navbar--fixed");
 })
 
 $(".desktop-nav__contents").click(function (e) {
@@ -13,20 +12,13 @@ $(".desktop-nav__contents").click(function (e) {
 $(".desktop-modal-header").click(function (e) {
   e.stopPropagation();
 })
-
-// var isClicked = $('.scholarship-link').data('clicked');
-// if (isClicked == 'yes') {
-// do something
 ScrollTrigger.create({
   start: 'top -40',
   end: 99999,
   duration: 2,
   toggleClass: { className: 'custom-navbar--fixed', targets: '.normal-nav-header' }
 });
-// }
 
 $(".scholarship-link").click(function () {
-  // $(".custom-navbar").toggleClass("custom-navbar--fixed");
   $(".desktop-nav").toggleClass("d-lg-block");
-  // $(".desktop-nav").toggleClass("custom-navbar--fixed");
 })
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,9 +1,3 @@
 $(".navbar-toggler").click(function () {
-  $(".mobile-nav").toggleClass("d-none");
-  // $(".custom-navbar").toggleClass("custom-navbar--fixed-mobile")
+  $(".mobile-nav").toggleClass("mobile-nav--transition");
 });
-
-// $(".scholarship-link").click(function () {
-//   $(".custom-navbar").toggleClass("custom-navbar--fixed");
-//   $(".desktop-nav").toggleClass("d-lg-block");
-// })
